57af829ac07a53217b60ddeece5076ff3ac7b2b5
borsch <bborsch.24@gmail.com>
2015-01-24 21:57:42 +0200

    graph

---
+++ Graph.java
@@ -0,0 +1,37 @@
+public class Graph {
+	
+	private final int V;
+	private int E;
+	private Bag<Integer>[] adj;
+	
+	public Graph(int V){
+		this.V = V;
+		adj = (Bag<Integer>[]) new Bag[V];
+		for (int v=0; v<V; v++)
+			adj[v] = new Bag<Integer>();
+	}
+	
+	public void addEdge(int v, int w){
+		adj[v].add(w);
+		adj[w].add(v);
+	}
+	
+	public int degree(int v){
+		int degree = 0;
+		for (int w : adj(v))
+			degree++;
+		return degree;
+	}
+	
+	public int getVertexCount(){
+		return V;
+	}
+	
+	public int getEdgeCounter(){
+		return E;
+	}
+	
+	public Iterable<Integer> adj(int v){
+		return adj[v];
+	}
+}
